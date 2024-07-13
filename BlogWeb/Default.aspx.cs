ede90fee6c3364c9bcaa9a926fc65ee23fde1f0d
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-07-13 12:27:45 +0300

    Add project files.

---
+++ BlogWeb/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BlogWeb
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSetTableAdapters.TBLHAKKIMDATableAdapter dt = new DataSetTableAdapters.TBLHAKKIMDATableAdapter();
+            Repeater1.DataSource = dt.HakkimdaListele();
+            Repeater1.DataBind();
+
+            DataSetTableAdapters.TBLDENEYIMTableAdapter dt2 = new DataSetTableAdapters.TBLDENEYIMTableAdapter();
+            Repeater2.DataSource = dt2.DeneyimListele();
+            Repeater2.DataBind();
+
+            DataSetTableAdapters.TBLEGITIMTableAdapter dt3 = new DataSetTableAdapters.TBLEGITIMTableAdapter();
+            Repeater3.DataSource = dt3.EgitimListesi();
+            Repeater3.DataBind();
+
+            DataSetTableAdapters.TBLYETENEKLERTableAdapter dt4 = new DataSetTableAdapters.TBLYETENEKLERTableAdapter();
+            Repeater4.DataSource = dt4.YetenekListele();
+            Repeater4.DataBind();
+
+
+            DataSetTableAdapters.TBLHOBILERTableAdapter dt5 = new DataSetTableAdapters.TBLHOBILERTableAdapter();
+            Repeater5.DataSource = dt5.HobiListele();
+            Repeater5.DataBind();
+
+
+            DataSetTableAdapters.TBLPROJELERTableAdapter dt6 = new DataSetTableAdapters.TBLPROJELERTableAdapter();
+            Repeater6.DataSource = dt6.ProjeListele();
+            Repeater6.DataBind();
+        }
+    }
+}
---
+++ BlogWeb/Hakkimda.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin.Master" AutoEventWireup="true" CodeBehind="Hakkimda.aspx.cs" Inherits="BlogWeb.Hakkimda" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+    Değişicek Alan
+</asp:Content>
